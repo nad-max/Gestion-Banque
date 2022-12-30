caa146736eb33248051a4c9040c2ee1d878f8916
Medini Nada <nada.medini@etudiant-fsegt.utm.tn>
2022-12-30 19:56:21 +0100

    card + cheque

--- src/app/component/add-card/add-card.component.ts
+++ src/app/component/add-card/add-card.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-add-card',
   templateUrl: './add-card.component.html',
   styleUrls: ['./add-card.component.scss']
 })
 export class AddCardComponent implements OnInit {
-
-  constructor() { }
+  types: any = ['','Carte Classique','Carte Gold','Carte Tawfir','Carte Sayedati'];
+  constructor() { 
+   
+  }
 
   ngOnInit(): void {
   }
